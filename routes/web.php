a49645a2494258e381e274a3cb17a84c27864cbb
NAUFALZULHM <l200220211@student.ums.ac.id>
2024-10-19 11:56:10 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+Route::get('/template', function () {
+    return view('template');
+});
+
+Route::get('/', function () {
+    return view('admin.layouts.wrapper');
+});
