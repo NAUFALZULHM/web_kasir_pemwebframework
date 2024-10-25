7ef264b9a8d3e8f9cf0ea88711b028708c605f23
NAUFALZULHM <l200220211@student.ums.ac.id>
2024-10-25 21:49:26 +0700

    Menambah data dan edit data user

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 class AdminUserController extends Controller
 {
     /**
@@ -13,6 +14,7 @@ class AdminUserController extends Controller
     {
         //
         $data = [
+            'user' => User::get(),
             'content' => 'admin.user.index'
         ];
         return view('admin.layouts.wrapper', $data);
@@ -36,6 +38,17 @@ class AdminUserController extends Controller
     public function store(Request $request)
     {
         //
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            're_password' => 'required|same:password',
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+
+        User::create($data);
+        return redirect('/admin/user');
     }
 
     /**
@@ -52,6 +65,11 @@ class AdminUserController extends Controller
     public function edit(string $id)
     {
         //
+        $data = [
+            'user'    => User::find($id),
+            'content' => 'admin.user.create'
+        ];
+        return view('admin.layouts.wrapper', $data);
     }
 
     /**
@@ -60,6 +78,22 @@ class AdminUserController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $user = User::find($id);
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            // 'password' => 'required',
+            're_password' => 'same:password',
+        ]);
+
+        if($request->password !=''){
+            $data['password'] = Hash::make($request->password);
+        } else {
+            $data['password'] = $user->password;
+        }
+        
+        $user->update($data);
+        return redirect('/admin/user');
     }
 
     /**
--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -4,24 +4,48 @@
             <div class="card">
                 <div class="card-body">
                     <h4><b>Tambah Data</b></h4>
-
+                    @isset($user)
+                        <form action="/admin/user/{{ $user->id }}" method="POST">
+                            @method('put')
+                    @else
                     <form action="/admin/user" method="POST">
+                    @endisset
                         @csrf <!--Cross Site Request Forgery) untuk keamanan-->
                         <div class="form-group">
                             <label for=""><b>Nama Lengkap</b></label>
-                            <input type="text" class="form-control" name="name" placeholder="Nama Lengkap">
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Nama Lengkap" value="{{ isset($user) ? $user->name : ''}}"> 
+                            @error('name') <!--tulisan error-->
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for=""><b>Email</b></label>
-                            <input type="email" class="form-control" name="email" placeholder="Email">
+                            <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Email" value="{{ isset($user) ? $user->email : '' }}">
+                            @error('email') <!--tulisan error-->
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>  
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for=""><b>Password</b></label>
-                            <input type="password" class="form-control" name="password" placeholder="Password">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password">
+                            @error('password')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for=""><b>Konfirmasi Password</b></label>
-                            <input type="password" class="form-control" name="re_password" placeholder="Konfirmasi Password">
+                            <input type="password" class="form-control @error('re_password') is-invalid @enderror" name="re_password" placeholder="Konfirmasi Password">
+                            @error('re_password')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <a href="/admin/user" class="btn btn-secondary"><i class="fas fa-arrow-left"></i> Kembali</a>
                         <button class="btn btn-primary">Simpan</button>
