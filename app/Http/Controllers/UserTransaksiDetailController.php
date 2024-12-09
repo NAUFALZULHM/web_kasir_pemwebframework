08ce2e5ccd380653e58ee64440fbb49c53832c94
NAUFALZULHM <naufalmaulanasans@gmail.com>
2024-12-09 22:25:16 +0700

    bug cart: Memperbaiki bug di cart/transaksi

--- app/Http/Controllers/UserTransaksiDetailController.php
+++ app/Http/Controllers/UserTransaksiDetailController.php
@@ -12,17 +12,28 @@ class UserTransaksiDetailController extends Controller
     {
         // die('masuk');
         // dd($request->all());
-        $data = [
-            'produk_id' => $request->produk_id,
-            'produk_name' => $request->produk_name,
-            'transaksi_id' => $request->transaksi_id,
-            'qty' => $request->qty,
-            'subtotal' => $request->subtotal,
-        ];
-        TransaksiDetail::create($data);
-        return redirect()->back();
-        
+        $produk_id = $request->produk_id;
+        $transaksi_id = $request->transaksi_id;
 
+        $td = TransaksiDetail::whereProdukId($produk_id)->whereTransaksiId($transaksi_id)->first();
+
+        if($td == null) {
+            $data = [
+                'produk_id' => $produk_id,
+                'produk_name' => $request->produk_name,
+                'transaksi_id' => $request->transaksi_id,
+                'qty' => $request->qty,
+                'subtotal' => $request->subtotal,
+            ];
+            TransaksiDetail::create($data);
+        }else{
+            $data = [
+                'qty' => $td->qty + $request->qty,
+                'subtotal' => $request->subtotal + $td->subtotal,
+            ];
+            $td->update($data);
+        }
+        return redirect('transaksi/'.$transaksi_id.'/edit');
     }
 
     // public function show($transaksi_id)
